86a3f0ec7ea73e4541f0e5e696bb778ec2a535e5
otaki <ohatakky@gmail.com>
2019-03-19 02:00:43 +0900

    goでは名前空間の競合は、パッケージの利用者が指定する必要がある

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"calcFeeSample/external"
+	ex_mock "calcFeeSample/external/mock"
 	"calcFeeSample/internal"
 	"calcFeeSample/internal/mock"
+	in_mock "calcFeeSample/internal/mock"
 	"fmt"
 )
 
 func main() {
-	ex_mock := mock.CalcFee{}
+	ex_mock := ex_mock.CalcFee{}
 	ex := external.External{CalcFee: ex_mock}
 	fmt.Println(ex)
 
-	in_mock_cf := mock.CalcFee{}
+	in_mock_cf := in_mock.CalcFee{}
 	in_mock_pr := mock.PaymentReport{}
 	in := internal.Internal{CalcFee: in_mock_cf, PaymentReport: in_mock_pr}
 	fmt.Println(in)
